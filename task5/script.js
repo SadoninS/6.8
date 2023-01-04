ffb0db6afa8a86e5f001f4c3bcad25f430d03ab7
SadoninS <stanislavsadonin@gmail.com>
2023-01-04 21:48:23 +0400

    add script to task 5

--- task5/script.js
+++ task5/script.js
@@ -1,15 +1,32 @@
-
+let outputText = document.querySelector('#duplicate');
 let input = document.querySelector('input');
 
-input.addEventListener('keypress', function(output) {
-  let outputText = document.querySelector('#duplicate')
+input.addEventListener('input', function(output) {
   outputText.textContent = output.target.value
 })
 
-let outputText = document.querySelector('#duplicate');
 document.querySelector('button').addEventListener('click', function(submit) {
     submit.preventDefault();
-    console.log(outputText);
+    outputText.textContent = "";
+    console.log(input.value);
+    input.value = "";
+    
 } )
 
- 
+
+ // оставил закоментированным код, поправленный Видякиным Алексеем, постарался на его основе доработать свой скрипт, чтобы все работало.
+
+// let input = document.querySelector("input");
+// let outputText = document.querySelector("#duplicate");
+// let button = document.querySelector("button");
+
+// input.addEventListener("input", function () {
+//   outputText.textContent = input.value;
+// });
+
+// button.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   outputText.textContent = "";
+//   console.log(input.value);
+//   input.value = "";
+// });
